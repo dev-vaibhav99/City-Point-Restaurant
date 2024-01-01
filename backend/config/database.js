8e64c738484e7433727a530680a463143db04765
Vaibhav <dev.vaibhav99@gmail.com>
2024-01-02 00:39:14 +0530

    Hardcode every env variables

--- backend/config/database.js
+++ backend/config/database.js
@@ -18,10 +18,10 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    username: process.env.DB_USER || "u904598717_hotel_mgmt",
+    password: process.env.DB_PASSWORD || "Hotel#Mgmt@123",
+    database: process.env.DB_NAME || "u904598717_hotel_mgmt",
+    host: process.env.DB_HOST || "srv944.hstgr.io",
+    dialect: process.env.DB_DIALECT || "mysql",
   },
 };
