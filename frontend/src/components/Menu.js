902cc2ed169773995163c1c3323d634df10e6d50
Vaibhav <dev.vaibhav99@gmail.com>
2024-01-01 17:13:36 +0530

    Uploading project for demo

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -26,121 +26,115 @@ const Menu = ({ history }) => {
     };
 
     return (
-        <aside className="main-sidebar sidebar-dark-primary elevation-4">
-            {/* Brand Logo */}
-            <Link to={"/dashboard"} className="brand-link">
-                <img
-                    src="/logo.png"
-                    alt="AdminLTE Logo"
-                    className="brand-image img-circle elevation-3"
-                    style={{ opacity: ".8" }}
-                />
-                <span className="brand-text font-weight-light">Restobar</span>
-            </Link>
-            {/* Sidebar */}
-            <div className="sidebar">
-                {/* Sidebar user panel (optional) */}
-                <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div className="image">
-                        <img
-                            src={userInfo ? userInfo.image : "/avatar.png"}
-                            className="img-circle elevation-2"
-                            alt="User"
-                        />
-                    </div>
-                    <div className="info">
-                        <Link to="/profile" className="d-block">
-                            {userInfo ? userInfo.name : ""}
-                        </Link>
-                    </div>
-                </div>
-                {/* Sidebar Menu */}
-                <nav className="mt-2">
-                    <ul
-                        className="nav nav-pills nav-sidebar flex-column"
-                        data-widget="treeview"
-                        role="menu"
-                        data-accordion="false"
-                    >
-                        <li className="nav-item">
-                            <Link to="/dashboard" className="nav-link">
-                                <i className="nav-icon fas fa-tachometer-alt" />{" "}
-                                <p> Dashboard</p>
-                            </Link>
-                        </li>
-
-                        {!userInfo ? (
-                            ""
-                        ) : userInfo.isAdmin === true ? (
-                            <>
-                                <li className="nav-header">ADMIN</li>
-                                <li className="nav-item">
-                                    <Link to="/user" className="nav-link">
-                                        <i className="nav-icon fas fa-users" />{" "}
-                                        <p> Users</p>
-                                    </Link>
-                                </li>
-                            </>
-                        ) : (
-                            ""
-                        )}
-
-                        <li className="nav-header">RESTOBAR</li>
-                        <li className="nav-item">
-                            <Link to="/active" className="nav-link">
-                                <i className="nav-icon fas fa-bell" />{" "}
-                                <p> Active Orders</p>
-                            </Link>
-                        </li>
-
-                        <li className="nav-item">
-                            <Link to="/delivery" className="nav-link">
-                                <i className="nav-icon fas fa-truck" />{" "}
-                                <p> Delivery</p>
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/order" className="nav-link">
-                                <i className="nav-icon far fa-clipboard" />{" "}
-                                <p> Orders</p>
-                            </Link>
-                        </li>
-
-                        <li className="nav-header">MANAGEMENT</li>
-
-                        <li className="nav-item">
-                            <Link to="/category" className="nav-link">
-                                <i className="nav-icon fas fa-list-alt" />{" "}
-                                <p> Categories</p>
-                            </Link>
-                        </li>
-
-                        <li className="nav-item">
-                            <Link to="/product" className="nav-link">
-                                <i className="nav-icon fas fa-hamburger" />{" "}
-                                <p> Products</p>
-                            </Link>
-                        </li>
-
-                        <li className="nav-item">
-                            <Link to="/client" className="nav-link">
-                                <i className="nav-icon fas fa-user" />{" "}
-                                <p> Clients</p>
-                            </Link>
-                        </li>
-
-                        <li className="nav-item">
-                            <Link to="/table" className="nav-link">
-                                <i className="nav-icon fas fa-border-all" />{" "}
-                                <p> Tables</p>
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-                {/* /.sidebar-menu */}
+      <aside className="main-sidebar sidebar-dark-primary elevation-4">
+        {/* Brand Logo */}
+        <Link to={"/dashboard"} className="brand-link">
+          <img
+            src="/logo.png"
+            alt="AdminLTE Logo"
+            className="brand-image img-circle elevation-3"
+            style={{ opacity: ".8" }}
+          />
+          <span className="brand-text font-weight-light">
+            City Point Restaurant
+          </span>
+        </Link>
+        {/* Sidebar */}
+        <div className="sidebar">
+          {/* Sidebar user panel (optional) */}
+          <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div className="image">
+              <img
+                src={userInfo ? userInfo.image : "/avatar.png"}
+                className="img-circle elevation-2"
+                alt="User"
+              />
             </div>
-            {/* /.sidebar */}
-        </aside>
+            <div className="info">
+              <Link to="/profile" className="d-block">
+                {userInfo ? userInfo.name : ""}
+              </Link>
+            </div>
+          </div>
+          {/* Sidebar Menu */}
+          <nav className="mt-2">
+            <ul
+              className="nav nav-pills nav-sidebar flex-column"
+              data-widget="treeview"
+              role="menu"
+              data-accordion="false"
+            >
+              <li className="nav-item">
+                <Link to="/dashboard" className="nav-link">
+                  <i className="nav-icon fas fa-tachometer-alt" />{" "}
+                  <p> Dashboard</p>
+                </Link>
+              </li>
+
+              {!userInfo ? (
+                ""
+              ) : userInfo.isAdmin === true ? (
+                <>
+                  <li className="nav-header">ADMIN</li>
+                  <li className="nav-item">
+                    <Link to="/user" className="nav-link">
+                      <i className="nav-icon fas fa-users" /> <p> Users</p>
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                ""
+              )}
+
+              <li className="nav-header">RESTOBAR</li>
+              <li className="nav-item">
+                <Link to="/active" className="nav-link">
+                  <i className="nav-icon fas fa-bell" /> <p> Active Orders</p>
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/delivery" className="nav-link">
+                  <i className="nav-icon fas fa-truck" /> <p> Delivery</p>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/order" className="nav-link">
+                  <i className="nav-icon far fa-clipboard" /> <p> Orders</p>
+                </Link>
+              </li>
+
+              <li className="nav-header">MANAGEMENT</li>
+
+              <li className="nav-item">
+                <Link to="/category" className="nav-link">
+                  <i className="nav-icon fas fa-list-alt" /> <p> Categories</p>
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/product" className="nav-link">
+                  <i className="nav-icon fas fa-hamburger" /> <p> Products</p>
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/client" className="nav-link">
+                  <i className="nav-icon fas fa-user" /> <p> Clients</p>
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/table" className="nav-link">
+                  <i className="nav-icon fas fa-border-all" /> <p> Tables</p>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          {/* /.sidebar-menu */}
+        </div>
+        {/* /.sidebar */}
+      </aside>
     );
 };
 
