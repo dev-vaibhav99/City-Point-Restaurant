9be3f5266aa2a20e5c1f797f1158a69a155eb12d
Vaibhav <dev.vaibhav99@gmail.com>
2024-01-02 00:14:28 +0530

    Try adding host in if env variables not provided

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,7 +7,7 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || "srv944.hstgr.io",
     dialect: process.env.DB_DIALECT || "mysql",
   },
   test: {
