47a9d094268fc9720f57247afefcd9b3782b93b0
Vaibhav <dev.vaibhav99@gmail.com>
2024-01-03 21:25:28 +0530

    Comment swagger-jsdoc import and usage

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const path = require("path");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+// const swaggerJSDoc = require("swagger-jsdoc");
+// const swaggerUI = require("swagger-ui-express");
 
 dotenv.config();
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
+  app.use(morgan("dev"));
 }
 
 app.use(express.json()); // for parsing application/json
@@ -22,6 +24,39 @@ const clientRoutes = require("./routes/client");
 const tableRoutes = require("./routes/table");
 const orderRoutes = require("./routes/order");
 const uploadRoutes = require("./routes/upload");
+
+/* Swagger configuration */
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "City Point Restaurant",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        api: "http://localhost:5000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+// const swaggerSpec = swaggerJSDoc(options);
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+/* Handling CORS */
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
 
 /* Routes */
 app.use("/api/users", userRoutes);
