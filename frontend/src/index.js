7e3ac0c57d1ad1f9875a395a25379c1fa6bd8200
Vaibhav <dev.vaibhav99@gmail.com>
2024-01-06 15:56:48 +0530

    Add the default base url and lint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {Provider} from 'react-redux'
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import store from './store';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import store from "./store";
+import axios from "axios";
 
+axios.defaults.baseURL = "https://citypointrestaurant.onrender.com";
 
 ReactDOM.render(
-
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
